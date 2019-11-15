eadb2892dce5e14c7a887d65ebd76b84520c72ec
evansz12 <43017757+evansz12@users.noreply.github.com>
2019-11-14 17:10:06 -0800

    Update complexLogic.js

--- bin/complexLogic/complexLogic.js
+++ bin/complexLogic/complexLogic.js
@@ -1,74 +1,164 @@
-//var adjKey = ["Starbucks", "Village", "Triple O's", "Shopper's", "Tim Horton's"];
-var adjMatrix = [
-  [0, 1100, 300, 2000, 1400],
-  [1100, 0, 1200, 3000, 2300],
-  [300, 1200, 0, 2200, 1500],
-  [2000, 3000, 2200, 0, 2900],
-  [1400, 2300, 1500, 2900, 0]
-];
-var adjLoc = [
-  {x:45, y:64}, 
-  {x:45, y:64}, 
-  {x:45, y:64}, 
-  {x:45, y:64},
-  {x:45, y:64}
-];
-/*
-startPos: starting position of courier
-twoVisit: 2D array of locations to visit
-custLoc: 2D array of customer loactions, with respect to toVisit orders, ie. toVisit[1] contains orders of custLoc[1]
-returns the shortest possible path to pickup deliveries and get to customer
-*/
-const solve = (startPos, twoVisit) => {
-  //adds all non repeating points to new 1D array
-  var toVisit = new Array();
-  for (var i = 0; i < twoVisit.length; i++) {
-    for (var j = 0; j < twoVisit[i].length; j++) {
-      if (!toVisit.includes(twoVisit[i][j])) {
-        toVisit.push(twoVisit[i][j]);
-      }
-    }
-  } 
-  toVisit = permute(startPos, toVisit, 0, Number.MAX_SAFE_INTEGER); 
+//var orderFormat = {id, userid, courierid, content, lat, lng, deslat, deslng, status, time}; 
+//var startPos = {lat:0, lng: 0};
 
-  return toVisit;
+/* 
+ * Traveling salesman pathfinding
+ */
+const pathFinding = (getOrders, lat, lng) => {
+  if (getOrders.length == 0) {
+    return getOrders; //return empty array
+  }
+  let startPos = {lat: lat, lng: lng};
+  
+  let allOrders = [];
+  for (let i = 0; i < getOrders.length; i++) {
+    if (getOrders[i].status == 0) {
+      allOrders.push(getOrders[i]);     
+    }
+  }
+  if (allOrders.length == 0) {
+    return allOrders; //return empty array
+  }
+
+  let desArray = [], custArray = [];
+  for (let i = 0; i < allOrders.length; i++) {               
+    let loc = {lat: allOrders[i].deslat, lng: allOrders[i].deslng};
+    desArray.push(loc);
+    loc = {lat: allOrders[i].lat, lng: allOrders[i].lng};
+    custArray.push(loc);
+  } 
+  //removes repeating locations
+  desArray = desArray.filter((desArray, index, self) =>   
+    index === self.findIndex((t) => (
+      t.lat === desArray.lat && t.lng === desArray.lng
+    ))
+  )
+  custArray = custArray.filter((custArray, index, self) =>
+    index === self.findIndex((t) => (
+      t.lat === custArray.lat && t.lng === custArray.lng
+    ))
+  )
+  //finds min weight path first for locations, then for destination
+  desArray = permute(desArray);   
+  desArray = findBest(startPos, desArray);
+
+  custArray = permute(custArray); 
+  custArray = findBest(desArray[desArray.length - 1], custArray);
+  //adds courier starting location and merges into 1 array
+  desArray.unshift(startPos);            
+  desArray = desArray.concat(custArray);
+
+  return desArray;
 }
 
-const permute = (startPos, toVisit, l, minWeight) => { 
-  if (l == toVisit.length - 1) {
-    //gets the starting distance and the distance of points to visit in order
-    var weight = getWeight(toVisit) + getDist(startPos, toVisit[0]);
+const permute = (toVisit) => {
+  let permArray = [];
+
+  for (let i = 0; i < toVisit.length; i = i + 1) {
+    let rest = permute(toVisit.slice(0, i).concat(toVisit.slice(i + 1)));
+
+    if(!rest.length) {
+      permArray.push([toVisit[i]])
+    } else {
+      for(let j = 0; j < rest.length; j = j + 1) {
+        permArray.push([toVisit[i]].concat(rest[j]))
+      }
+    }
+  }
+  return permArray;
+}
+
+const findBest = (startPos, toVisit) => {
+  let minWeight = Number.MAX_SAFE_INTEGER;
+  let minRoute = [];
+  for (let i = 0; i < toVisit.length; i++) {
+    let weight = getWeight(toVisit[i]) + getDist(startPos, toVisit[i][0]);
     if (weight < minWeight) {
       minWeight = weight;    
-      var minRoute = [...toVisit];
+      minRoute = [...toVisit[i]];      
     }
-  } 
-  else { 
-    for (var i = l; i < toVisit.length; i++) {
-      if (l !== i) {
-        let temp = toVisit[l];
-        toVisit[l] = toVisit[i];
-        toVisit[i] = temp;        
-        minRoute = permute(startPos, toVisit, l+1, minWeight); 
-        temp = toVisit[l];
-        toVisit[l] = toVisit[i];
-        toVisit[i] = temp;        
-      } else {
-        minRoute = permute(startPos, toVisit, l+1, minWeight); 
-      }
-    } 
-  } 
-  return minRoute;
+  }
+  return minRoute; 
 }
-
 const getWeight = (toVisit) => {
   var weight = 0;
-  for (var i = 0; i < toVisit.length - 1; i++) {    
-    weight += adjMatrix[toVisit[i]][toVisit[i+1]];
+  for (let i = 0; i < toVisit.length - 1; i++) {    
+    weight += getDist(toVisit[i], toVisit[i + 1]);
   }
   return weight;
 }
 
-const getDist = (point, index) => Math.sqrt(Math.pow(point.x - adjLoc[index].x, 2) + Math.pow(point.y - adjLoc[index].y, 2));
+const getDist = (start, end) => Math.sqrt(Math.pow(start.lat - end.lat, 2) + Math.pow(start.lng - end.lng, 2));
 
-module.exports = {"solve": solve};
+/*  
+ * return an array of sorted orders id
+ */
+const sortOrder = (allOrders, lat, lng) => {
+  if (allOrders.length == 0) {
+    return allOrders; //return empty array
+  }
+  let startPos = {lat: lat, lng: lng};
+
+  var available = [], current = [];
+  for (let i = 0; i < allOrders.length; i++) {
+    if (allOrders[i].status == 1) {
+      available.push(allOrders[i]);
+    } 
+    else if (allOrders[i].status == 0) {
+      current.push(allOrders[i]);     
+    }
+  }
+  var i = 0;
+  while(i < available.length){
+    var today = new Date();
+    var time = today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
+    var curTime = parseInt(time[0])*60 + parseInt(time[1]);
+    var itimeArr =available[i].time.split(':');
+    var itime = parseInt(itimeArr[0])*60 + parseInt(itimeArr[1]) - curTime;
+    if(itime<-30){
+      available.split(i,1);
+      i--;
+    }
+    i++;
+  }
+  available = mergeSort(startPos, available);  
+  return current.concat(available);        
+}
+
+const mergeSort = (startPos, available) => {
+  if(available.length <= 1){
+    return available;
+  }
+  const mid = Math.floor(available.length / 2);
+  var left = available.slice(0, mid);
+  var right = available.slice(mid);
+
+  return merge(mergeSort(startPos, left), mergeSort(startPos, right), startPos);
+}
+
+const merge = (L, R, startPos) => {
+  let result = []; i = 0; j = 0;
+  var today = new Date();
+  var time = today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
+  var curTime = parseInt(time[0])*60 + parseInt(time[1]);
+  while(i < L.length && j < R.length){
+      var iloc = {lat:L[i].deslat, lng:L[i].deslng};
+      var itimeArr =L[i].time.split(':');
+      var itime = Math.abs( parseInt(itimeArr[0])*60 + parseInt(itimeArr[1]) - curTime);
+      var iref = getDist(startPos, iloc) * 1000 + itime; 
+      var jloc = {lat:R[j].deslat, lng:R[j].deslng};
+      var jtimeArr =R[j].time.split(':');
+      var jtime = Math.abs(parseInt(jtimeArr[0])*60 + parseInt(jtimeArr[1]) - curTime);
+      var jref = getDist(startPos, jloc) * 1000 + jtime;
+    if(iref < jref){
+      result.push(L[i]);
+      i++;
+    }else{
+      result.push(R[j]);
+      j++;
+    }
+  }
+  return result.concat(L.slice(i).concat(R.slice(j)));
+}
+
+module.exports = {pathFinding, sortOrder};
