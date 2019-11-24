6d6c3b72f7f21c4cf211b4a2332255cec014ec07
yanhua111 <609298031@qq.com>
2019-11-24 00:19:31 +0000

    navigator updated

--- front-end/src/utils/navigators.js
+++ front-end/src/utils/navigators.js
@@ -1,121 +1,127 @@
-import { createAppContainer, createSwitchNavigator,createDrawerNavigator} from 'react-navigation';
-import {createStackNavigator,} from 'react-navigation-stack';
-import {createBottomTabNavigator,} from 'react-navigation-tabs';
+import { createAppContainer, createSwitchNavigator, createDrawerNavigator } from 'react-navigation';
+import { createStackNavigator  } from 'react-navigation-stack';
+import { createBottomTabNavigator  } from 'react-navigation-tabs';
 
-import AddressScreen from "../../screens/address";
-import CourierMap from "../../screens/couriermap";
-import CustomerScreen from "../../screens/customerscreen";
-import OpenPage from "../../screens/OpenPage";
-import OrderList from "../../screens/OrderList";
-import OrderScreen from "../../screens/PlaceorderPage";
-import Setting from "../../screens/setting";
-import ChangeSetting from "../../screens/ChangeSetting";
-import CustomerList from "../../screens/customerlist";
-import HistoryOrder from "../../screens/HistoryOrder";
+import AddressScreen from '../../screens/address';
+import CourierMap from '../../screens/couriermap';
+import CustomerScreen from '../../screens/customerscreen';
+import OpenPage from '../../screens/OpenPage';
+import OrderList from '../../screens/OrderList';
+import OrderScreen from '../../screens/PlaceorderPage';
+import Setting from '../../screens/setting';
+import ChangeSetting from '../../screens/ChangeSetting';
+import CustomerList from '../../screens/customerlist';
+import HistoryOrder from '../../screens/HistoryOrder';
+import OrderDetail from '../../screens/OrderDetail';
 
 // import { setDetectionImagesAsync } from 'expo/build/AR';
 
 export const CourierMapStack = createStackNavigator({
-    CourierMap: CourierMap,
-    OrderList: OrderList ,
-  },
-  {
-    headerMode: 'none',
-    navigationOptions: {
-      headerVisible: false,
-    }
-   }
-  );
+  CourierMap: CourierMap,
+  OrderList: OrderList
+},
+{
+  headerMode: 'none',
+  navigationOptions: {
+    headerVisible: false
+  }
+}
+);
 
+export const CustomerScreenStack = createStackNavigator({ // change here
+  CustomerScreen: CustomerScreen,
+  OrderScreen: OrderScreen,
+  AddressScreen: AddressScreen
+}, {
+  headerMode: 'none',
+  navigationOptions: {
+    headerVisible: false
+  }
+}
+);
 
-  export const CustomerScreenStack = createStackNavigator({ //change here
-    CustomerScreen :CustomerScreen,
-    OrderScreen: OrderScreen,
-    AddressScreen: AddressScreen,
-  },{
-    headerMode: 'none',
-    navigationOptions: {
-      headerVisible: false,
-    }
-   }
-  );
-
-
-
-  export const addressSwitch = createSwitchNavigator({ 
-    AddressScreen: AddressScreen,
-    OrderScreen: OrderScreen ,
-  });
-
-  export const PlaceOrderStack = createStackNavigator({
-    PlaceOrder: OrderScreen,
-    addressSwitch: addressSwitch
-  },
-  {
-    headerMode: 'none',
-    navigationOptions: {
-      headerVisible: false,
-    }
-   }
-  );
-
-  export const SettingSwitch = createSwitchNavigator({ 
-    Setting: Setting,
-    ChangeSetting: ChangeSetting,
-  });
-  
-  export const CustomerListStack = createStackNavigator({
-    CustomerList: {screen: CustomerList},
-    HistoryOrder: {screen: HistoryOrder},
-  },
-  {
-    headerMode: 'none',
-    navigationOptions: {
-      headerVisible: false,
-    }
-   })
-
-  export const CourierTabs = createBottomTabNavigator({
-      OrderList: {screen: OrderList},
-      Map      : CourierMap,
-      //Setting:{screen : Setting},
-      Setting:  SettingSwitch,
-     
-  });
-
-  export const CustomerTabs = createBottomTabNavigator({
-    //CustomerScreen: {screen: CustomerScreen},
-    // PlaceOrder      : PlaceOrderStack,
-    //Setting:{screen : Setting},
-    PlaceOrder:CustomerScreenStack,
-    CustomerList:CustomerListStack, //{screen: CustomerList},
-    Setting:  SettingSwitch,
+export const addressSwitch = createSwitchNavigator({
+  AddressScreen: AddressScreen,
+  OrderScreen: OrderScreen
 });
-  
-   
+
+export const PlaceOrderStack = createStackNavigator({
+  PlaceOrder: OrderScreen,
+  addressSwitch: addressSwitch
+},
+{
+  headerMode: 'none',
+  navigationOptions: {
+    headerVisible: false
+  }
+}
+);
+
+export const SettingSwitch = createSwitchNavigator({
+  Setting: Setting,
+  ChangeSetting: ChangeSetting
+});
+
+export const CustomerListStack = createStackNavigator({
+  CustomerList: { screen: CustomerList },
+  HistoryOrder: { screen: HistoryOrder }
+},
+{
+  headerMode: 'none',
+  navigationOptions: {
+    headerVisible: false
+  }
+});
+
+export const OrderListStack = createStackNavigator({
+  OrderList: { screen: OrderList },
+  OrderDetail: { screen: OrderDetail}
+}, {
+  headerMode: 'none',
+  navigationOptions: {
+    headerVisible: false
+  }
+}
+);
+export const CourierTabs = createBottomTabNavigator({
+  // OrderList: { screen: OrderList },
+  OrderList: OrderListStack,
+  Map: CourierMap,
+  // Setting:{screen : Setting},
+  Setting: SettingSwitch
+
+});
+
+export const CustomerTabs = createBottomTabNavigator({
+  // CustomerScreen: {screen: CustomerScreen},
+  // PlaceOrder      : PlaceOrderStack,
+  // Setting:{screen : Setting},
+  PlaceOrder: CustomerScreenStack,
+  CustomerList: CustomerListStack, // {screen: CustomerList},
+  Setting: SettingSwitch
+});
+
+
 
 export const courierStack = createStackNavigator({
-  CourierTabs : CourierTabs,
-  //CourierMapStack: CourierMapStack,
-  //CourierMap: CourierMap,
+  CourierTabs: CourierTabs
+  // CourierMapStack: CourierMapStack,
+  // CourierMap: CourierMap,
 },
 {
   headerMode: 'none',
   navigationOptions: {
-    headerVisible: false,
+    headerVisible: false
   }
- })
+});
 
 export const customerStack = createStackNavigator({
-  CustomerTabs: CustomerTabs,
+  CustomerTabs: CustomerTabs
 },
 {
   headerMode: 'none',
   navigationOptions: {
-    headerVisible: false,
+    headerVisible: false
   }
- }
- )
-
-
-
+}
+);
