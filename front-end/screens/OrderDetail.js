5ee14b09efef334d97fcdf11c36c6cae4be90cae
yanhua111 <609298031@qq.com>
2019-11-24 04:39:08 +0000

    update order stuff

--- front-end/screens/OrderDetail.js
+++ front-end/screens/OrderDetail.js
@@ -129,18 +129,17 @@ export default class OrderDetail extends React.Component {
     var status = this.props.navigation.getParam("status", "");
     const id = this.props.navigation.getParam("id", "");
     const detail = this.props.navigation.getParam("detail", "");
-    const time = this.props.navigation.getParam("locFrom", "");
+    const time = this.props.navigation.getParam("time", "");
     const userid = this.props.navigation.getParam("userid", "");
     return (
       <View style={styles.container}>
-        <CustomLoading 
+        <CustomLoading
         visible={this.state.showloader}
         />
         <TopBar onBackPress={() => this.props.navigation.navigate("OrderList")}>
           Order Detail
         </TopBar>
         <OrderView
-          profilepic={profilepic}
           locFrom={locFrom}
           locTo={locTo}
           status={status}
